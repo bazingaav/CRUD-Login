f93ab3dc9752c67254888f43e16f119a4f8c2580
bazingaav <athashreeav5556@gmail.com>
2020-08-05 19:34:44 +0530

    designed navbar & sidebar, search fields & form.
    Till now:
    1. only front end is done.
    2. JS is pending
    3. BE+FE Connection is left

--- NodeJS/Controllers/student_details_con.js
+++ NodeJS/Controllers/student_details_con.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var app = express();
+var router = express.Router();
 
 var {student_details} = require('../Models/student_details');
 
@@ -7,7 +7,7 @@ var {student_details} = require('../Models/student_details');
 
 // localhost:3000/student_details
 /* Viewing all records in DB*/
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     let sql = 'SELECT * from student_details';
     student_details.all(sql,(err,docs) => {
         if(!err){
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 
 
 /* View record by id*/
-app.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => {
     var params = [req.params.id];
     let sql = 'SELECT * from student_details where id = ?';
     student_details.get(sql, params, (err, docs) => {
@@ -42,7 +42,7 @@ app.get("/:id", (req, res) => {
 
 
 /* Inserting data into DB */
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     var reqBody = req.body;
     let insert = 'INSERT into student_details (name,age,school,grade,div,status) VALUES (?,?,?,?,?,?)';
     let parameters = [reqBody.name,reqBody.age,reqBody.school,reqBody.grade,reqBody.div,reqBody.status];
@@ -60,7 +60,7 @@ app.post('/', (req, res) => {
 });
 
 /* Updating existing record */
-app.patch("/", (req, res, next) => {
+router.patch("/", (req, res, next) => {
     var reqBody = req.body;
     let update = `UPDATE student_details set name = ?, age = ?, school = ?, grade = ?, div = ?, status = ? WHERE id = ?`;
     let parameters = [reqBody.name,reqBody.age,reqBody.school,reqBody.grade,reqBody.div,reqBody.status,reqBody.id];
@@ -79,7 +79,7 @@ app.patch("/", (req, res, next) => {
 });
 
 /* Deleting records */
-app.delete("/:id", (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
     let del = 'DELETE from student_details WHERE id=?';
     let params = [req.params.id];
     student_details.run(del,params,(err,docs)=> {
@@ -94,4 +94,4 @@ app.delete("/:id", (req, res, next) => {
     });
 });
 
-module.exports = app;
+module.exports = router;
--- NodeJS/Models/student_details.js
+++ NodeJS/Models/student_details.js
@@ -1,18 +1,18 @@
 const sqlite3 = require('sqlite3');
 const db = require("../db")
 
-
-var student_details = db.run('CREATE TABLE IF NOT EXISTS student_details(\
+let create = 'CREATE TABLE IF NOT EXISTS student_details(\
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
     name VARCHAR(30) NOT NULL,\
     age INTEGER NOT NULL,\
     school VARCHAR(50) NOT NULL,\
     grade INTEGER NOT NULL,\
     div  CHARACTER(1) NOT NULL,\
-    status VARCHAR(10) NOT NULL\
-)',(err)=>{
+    status VARCHAR(10) NOT NULL)';
+ 
+var student_details = db.run(create, [] ,(err)=>{
     if(!err){
-        console.log("student_detils table created!");
+        console.log("student_details table created/exists!");
     }
     else{
         console.log('Error in student_details table creation: '+ JSON.stringify(err,undefined,2));
